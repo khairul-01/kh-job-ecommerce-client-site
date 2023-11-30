c9c82e03841289a908f11c7f9c4014e0d648da30
khairul-01 <khairul431743@gmail.com>
2023-11-30 18:55:48 +0600

    added accept and reject functionality for accept and rejact button

--- src/components/pages/BidRequests.jsx
+++ src/components/pages/BidRequests.jsx
@@ -1,11 +1,94 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
-import { AuthContext } from "../../provider/AuthProvider";
+import Swal from "sweetalert2";
+// import { AuthContext } from "../../provider/AuthProvider";
 
 
 const BidRequests = () => {
    // const {user} = useContext(AuthContext);
    const jobRequests = useLoaderData();
+   const handleAccept = (id) => {
+      const statusInfo = {
+         status: 'In Progress'
+      }
+      console.log(id, statusInfo);
+
+      Swal.fire({
+         title: "Are you sure to Accept this job?",
+         text: "You won't be able to revert this!",
+         icon: "warning",
+         showCancelButton: true,
+         confirmButtonColor: "#3085d6",
+         cancelButtonColor: "#d33",
+         confirmButtonText: "Yes, accept it!"
+      }).then((result) => {
+         if (result.isConfirmed) {
+            fetch(`http://localhost:5000/bidJobs/${id}`, {
+               method: 'PUT',
+               headers: {
+                  'content-type': 'application/json',
+               },
+               body: JSON.stringify(statusInfo)
+            })
+               .then(res => res.json())
+               .then(data => {
+                  console.log(data);
+                  if (data.modifiedCount > 0) {
+
+                     Swal.fire({
+                        title: 'Success!',
+                        text: 'Status information updated successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                     })
+
+                  }
+               })
+         }
+      })
+
+
+   }
+   const handleReject = (id) => {
+      const statusInfo = {
+         status: 'Rejected'
+      }
+      console.log(id, statusInfo);
+
+      Swal.fire({
+         title: "Are you sure to Reject this job?",
+         text: "You won't be able to revert this!",
+         icon: "warning",
+         showCancelButton: true,
+         confirmButtonColor: "#3085d6",
+         cancelButtonColor: "#d33",
+         confirmButtonText: "Yes, reject it!"
+      }).then((result) => {
+         if (result.isConfirmed) {
+            fetch(`http://localhost:5000/bidJobs/${id}`, {
+               method: 'PUT',
+               headers: {
+                  'content-type': 'application/json',
+               },
+               body: JSON.stringify(statusInfo)
+            })
+               .then(res => res.json())
+               .then(data => {
+                  console.log(data);
+                  if (data.modifiedCount > 0) {
+
+                     Swal.fire({
+                        title: 'Success!',
+                        text: 'Status information updated successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                     })
+
+                  }
+               })
+         }
+      })
+   }
    return (
       <div className="my-9">
          <h1 className="text-4xl text-center mb-5">Bid Requests</h1>
@@ -29,14 +112,14 @@ const BidRequests = () => {
 
                   {
                      jobRequests.map((bid, ind) => <tr key={ind} className="hover">
-                     <th>{ind+1}</th>
-                     <td>{bid.job_title}</td>
-                     <td>{bid.email}</td>
-                     <td>{bid.deadline}</td>
-                     <td>pending</td>
-                     <td><button className="btn">Accept</button></td>
-                     <td><button className="btn">Reject</button></td>
-                  </tr>)
+                        <th>{ind + 1}</th>
+                        <td>{bid.job_title}</td>
+                        <td>{bid.email}</td>
+                        <td>{bid.deadline}</td>
+                        <td>pending</td>
+                        <td><button onClick={() => handleAccept(bid._id)} className="btn">Accept</button></td>
+                        <td><button onClick={() => handleReject(bid._id)} className="btn">Reject</button></td>
+                     </tr>)
                   }
 
                </tbody>
